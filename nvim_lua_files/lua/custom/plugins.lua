af17e5b8b412b4a393bcabc27e48e0cb2beaf3f8
Niloy Majumder <niloym@cdac.in>
2024-01-31 16:02:48 +0530

    nvim configs

---
+++ nvim_lua_files/lua/custom/plugins.lua
@@ -0,0 +1,43 @@
+local plugins = {
+  -- {
+  --   "mhartington/formatter.nvim",
+  --   event = "VeryLazy",
+  --   opts = function ()
+  --     return require "custom.configs.formatter"
+  --   end
+  -- },
+  -- {
+  --   "mfussenegger/nvim-lint",
+  --   event = "VeryLazy",
+  --   config = function ()
+  --     require "custom.configs.lint"
+  --   end
+  -- },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    event = "VeryLazy",
+    opts = function ()
+      return require "custom.configs.null-ls"
+    end
+  },
+  {
+    "williamboman/mason.nvim",
+    opts ={
+      ensure_installed = {
+        "eslint-lsp",
+        "prettier",
+        "typescript-language-server"
+      }
+    }
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end
+  }
+
+}
+
+return plugins
