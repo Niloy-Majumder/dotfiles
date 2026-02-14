eebb4b8cabd9fe1f9806a96af39667539c632f39
Niloy Majumder <niloymajumder.98@gmail.com>
2026-02-14 10:28:57 +0530

    Lazy Vim Config Update

---
+++ lazy_nvim/nvim/lua/plugins/noice.lua
@@ -0,0 +1,10 @@
+return {
+  "folke/noice.nvim",
+  opts = {
+    lsp = {
+      progress = {
+        enabled = false, -- 🔴 THIS stops Publish Diagnostics / Validate Documents
+      },
+    },
+  },
+}
